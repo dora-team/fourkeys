f6474f2a3fc6e8a0fa3c0ffe7ad478485ca2341f
davidstanke <davidstanke@gmail.com>
2021-02-12 23:54:54 +0000

    start adding dict comparisons

--- data_generator/generate_data_test.py
+++ data_generator/generate_data_test.py
@@ -14,8 +14,43 @@
 
 import pytest
 import datetime
+import collections
 
 import generate_data
+
+def flatten(d,sep="_"):
+    obj = collections.OrderedDict()
+
+    def recurse(t,parent_key=""):
+        
+        if isinstance(t,list):
+            for i in range(len(t)):
+                recurse(t[i],parent_key + sep + str(i) if parent_key else str(i))
+        elif isinstance(t,dict):
+            for k,v in t.items():
+                recurse(v,parent_key + sep + k if parent_key else k)
+        else:
+            obj[parent_key] = t
+
+    recurse(d)
+
+    return obj
+
+
+def compare_dicts(dict_a, dict_b):
+    
+    # flatten any nested structures, so we only need one pass
+    flat_dict_a = flatten(dict_a)
+    flat_dict_b = flatten(dict_b)
+
+    for key in flat_dict_a:
+
+        assert type(flat_dict_a[key]) == type(flat_dict_b[key]), \
+            f"type mismatch comparing '{key}': {type(flat_dict_a[key]).__name__} != {type(flat_dict_b[key]).__name__}"
+
+        if isinstance(flat_dict_a[key], str):
+            assert len(flat_dict_a[key]) == len(flat_dict_b[key]), \
+                f"length mismatch comparing strings in '{key}': {len(flat_dict_a[key])} != {len(flat_dict_b[key])}"
 
 
 @pytest.fixture
@@ -28,20 +63,28 @@ def gitlab_changes():
     return generate_data.make_changes(2, "gitlab", 604800)
 
 
-def test_changes_github(github_changes):
+@pytest.fixture
+def valid_github_changes():
 
-    # example of valid output
-    # {'head_commit': {'id': 'c814b7082ba2ae5d2076568baa67a6b694845e42',
-    # 'timestamp': datetime.datetime(2021, 2, 1, 3, 38, 39, 923909)},
-    # 'commits': [
-    #   {'id': 'c814b7082ba2ae5d2076568baa67a6b694845e42',
-    #       'timestamp': datetime.datetime(2021, 2, 1, 3, 38, 39, 923909)},
-    #   {'id': '3328c2fd3be8f20a5e072681cc5b2c86e644b839',
-    #       'timestamp': datetime.datetime(2021, 1, 28, 10, 28, 32, 923935)}]}
+    # return an example of what valid data looks like
+    valid_github_changes = {
+        'head_commit': {
+            'id': '29f54bb6cdb25a67dc7a2b7dae17a1346e2e9609',
+            'timestamp': datetime.datetime(2021, 2, 1, 3, 38, 39, 923909)
+        },
+        'commits': [
+            {'id': 'c814b7082ba2ae5d2076568baa67a6b694845e42',
+             'timestamp': datetime.datetime(2021, 2, 1, 3, 38, 39, 923909)},
+            {'id': '29f54bb6cdb25a67dc7a2b7dae17a1346e2e9609',
+             'timestamp': datetime.datetime(2021, 1, 28, 10, 28, 32, 923935)}
+        ]
+    }
 
-    # spot-check a few parts of the generated data
-    assert len(github_changes['head_commit']['id']) == 40
-    assert isinstance(github_changes['commits'][len(github_changes['commits'])-1]['timestamp'], datetime.datetime)
+    return valid_github_changes
+
+
+def test_changes_github(github_changes, valid_github_changes):
+    compare_dicts(github_changes,valid_github_changes)
 
 
 def test_changes_gitlab(gitlab_changes):
@@ -56,7 +99,8 @@ def test_changes_gitlab(gitlab_changes):
 
     # spot-check a few parts of the generated data
     assert len(gitlab_changes['checkout_sha']) == 40
-    assert isinstance(gitlab_changes['commits'][len(gitlab_changes['commits'])-1]['timestamp'], datetime.datetime)
+    assert isinstance(gitlab_changes['commits'][len(
+        gitlab_changes['commits'])-1]['timestamp'], datetime.datetime)
 
 
 def test_deploy_github(github_changes):
@@ -67,10 +111,12 @@ def test_deploy_github(github_changes):
     #   'id': '14cdd47757a1ef343c4e183b457ff5cbe85a173b', 'state': 'success'},
     #   'deployment': {'sha': '189941869a9bee33fb03e1e18596ea55c4d892e2'}}
 
-    deployment = generate_data.create_github_deploy_event(github_changes['head_commit'])
+    deployment = generate_data.create_github_deploy_event(
+        github_changes['head_commit'])
 
     # spot check generated data
-    assert isinstance(deployment['deployment_status']['updated_at'], datetime.datetime)
+    assert isinstance(deployment['deployment_status']
+                      ['updated_at'], datetime.datetime)
     assert len(deployment['deployment_status']['id']) == 40
     assert len(deployment['deployment']['sha']) == 40
 
@@ -87,7 +133,8 @@ def test_deploy_gitlab(gitlab_changes):
     deployment = generate_data.create_gitlab_pipeline_event(gitlab_changes)
 
     # spot check generated data
-    assert isinstance(deployment['object_attributes']['created_at'], datetime.datetime)
+    assert isinstance(deployment['object_attributes']
+                      ['created_at'], datetime.datetime)
     assert deployment['object_attributes']['status'] == 'success'
     assert len(deployment['commit']['id']) == 40
 
@@ -127,8 +174,10 @@ def test_make_gitlab_issue(gitlab_changes):
     print(issue)
 
     assert issue['object_kind'] == 'issue'
-    assert isinstance(issue['object_attributes']['created_at'], datetime.datetime)
-    assert isinstance(issue['object_attributes']['closed_at'], datetime.datetime)
+    assert isinstance(issue['object_attributes']
+                      ['created_at'], datetime.datetime)
+    assert isinstance(issue['object_attributes']
+                      ['closed_at'], datetime.datetime)
     assert isinstance(issue['object_attributes']['id'], int)
     assert issue['object_attributes']['labels'][0]['title'] == 'Incident'
     assert issue['object_attributes']['description'].startswith('root cause: ')
@@ -147,11 +196,13 @@ def test_github_request(github_changes):
 
     assert request.headers['X-github-event'] == 'push'
     assert request.headers['X-hub-signature'].startswith('sha1=')
-    assert len(request.headers['X-hub-signature']) == 45  # ex: 'sha1=78492d6d232fafa4c58b31888cd131d1db9f5dd5'
+    # ex: 'sha1=78492d6d232fafa4c58b31888cd131d1db9f5dd5'
+    assert len(request.headers['X-hub-signature']) == 45
     assert request.headers['User-agent'] == 'GitHub-Hookshot/mock'
     assert request.headers['Content-type'] == 'application/json'
     assert request.headers['Mock'] is True
-    assert len(request.data) >= 40  # post body should contain at least one commit SHA
+    # post body should contain at least one commit SHA
+    assert len(request.data) >= 40
 
 
 def test_gitlab_request(gitlab_changes):
@@ -168,4 +219,5 @@ def test_gitlab_request(gitlab_changes):
     assert request.headers['X-gitlab-token'] == 'dummy_secret_string'
     assert request.headers['Content-type'] == 'application/json'
     assert request.headers['Mock'] is True
-    assert len(request.data) >= 40  # post body should contain at least one commit SHA
+    # post body should contain at least one commit SHA
+    assert len(request.data) >= 40
