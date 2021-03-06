82c9e032674304ceefabf3c319f01f9e7589fce0
davidstanke <davidstanke@gmail.com>
2021-03-06 02:58:24 +0000

    factor out project number variable

--- experimental/terraform/cloud_run_service/variables.tf
+++ experimental/terraform/cloud_run_service/variables.tf
@@ -2,10 +2,6 @@ variable "google_project_id" {
   type = string
 }
 
-# variable "google_project_number" {
-#   type=number
-# }
-
 variable "google_region" {
   type = string
 }
--- experimental/terraform/variables.tf
+++ experimental/terraform/variables.tf
@@ -1,9 +1,7 @@
 variable "google_project_id" {
   type = string
 }
-# variable "google_project_number" {
-#   type = number
-# }
+
 variable "google_region" {
   type = string
 }
