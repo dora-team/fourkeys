91716051047c3cc187f7bb8acab3a9c0815465d1
davidstanke <davidstanke@gmail.com>
2021-03-10 01:01:23 +0000

    add BQ tables

--- experimental/terraform/main.tf
+++ experimental/terraform/main.tf
@@ -6,6 +6,10 @@ resource "google_project_service" "run_api" {
   service = "run.googleapis.com"
 }
 
+resource "google_project_service" "bq_api" {
+  service = "bigquery.googleapis.com"
+}
+
 module "cloud_run_service" {
   source            = "./cloud_run_service"
   google_project_id = var.google_project_id
@@ -16,4 +20,15 @@ module "cloud_run_service" {
     google_project_service.run_api,
   ]
 
+}
+
+resource "google_bigquery_dataset" "four_keys" {
+  dataset_id = "four_keys"
+}
+
+resource "google_bigquery_table" "bq_table" {
+  for_each = toset(["events_raw","changes","deployments","incidents"])
+  dataset_id = google_bigquery_dataset.four_keys.dataset_id
+  table_id = each.key
+  schema = file("../../setup/${each.key}_schema.json")
 }
