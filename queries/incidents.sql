2e3438a5df8e61d837ed5407bfcd470a364efa6e
rhysemmas <rhys.emmas@uswitch.com>
2021-04-29 16:34:20 +0100

    Fix

--- queries/incidents.sql
+++ queries/incidents.sql
@@ -5,6 +5,6 @@ SELECT
   TIMESTAMP(JSON_EXTRACT_SCALAR(metadata, '$.time_opened')) as time_created,
   TIMESTAMP(JSON_EXTRACT_SCALAR(metadata, '$.time_closed')) as time_resolved,
   JSON_EXTRACT_STRING_ARRAY(metadata, '$.root_causes') as root_cause,
-FROM four_keys.historic_incident
+FROM four_keys.events_raw
 WHERE event_type LIKE "issue%"
 ;
