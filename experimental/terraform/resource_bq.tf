be85a7051cfe99b307cba64ed9d790cc4ca7c0f6
davidstanke <davidstanke@gmail.com>
2021-05-19 21:48:46 +0000

    remove schemas from bq tables (except events_raw)

--- experimental/terraform/resource_bq.tf
+++ experimental/terraform/resource_bq.tf
@@ -2,14 +2,21 @@ resource "google_bigquery_dataset" "four_keys" {
   dataset_id = "four_keys"
 }
 
-resource "google_bigquery_table" "bq_table" {
-  for_each   = toset(["events_raw", "changes", "deployments", "incidents"])
+# TODO remove "for_each" and hardcode
+resource "google_bigquery_table" "bq_table_events_raw" {
+  for_each   = toset(["events_raw"])
   dataset_id = google_bigquery_dataset.four_keys.dataset_id
   table_id   = each.key
   schema     = file("../../setup/${each.key}_schema.json")
 }
 
-resource "google_bigquery_data_transfer_config" "scheduled_query" {
+resource "google_bigquery_table" "bq_tables_derived" {
+  for_each   = toset(["changes", "deployments", "incidents"])
+  dataset_id = google_bigquery_dataset.four_keys.dataset_id
+  table_id   = each.key
+}
+
+resource "google_bigquery_data_transfer_config" "scheduled_query1" {
 
   for_each = toset(["changes", "deployments", "incidents"])
 
@@ -22,5 +29,5 @@ resource "google_bigquery_data_transfer_config" "scheduled_query" {
     write_disposition               = "WRITE_TRUNCATE"
     query                           = file("../../queries/${each.key}.sql")
   }
-  depends_on = [google_project_service.bq_dt_api, google_bigquery_table.bq_table]
+  depends_on = [google_project_service.bq_dt_api, google_bigquery_table.bq_table_events_raw, google_bigquery_table.bq_tables_derived]
 }
