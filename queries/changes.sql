a5e4eb240bd43e73d9b09d837e5e3c67690b1b74
Philip Dalen <philip.harmse@gmail.com>
2021-05-26 11:41:11 +0200

    Unpack commits from metadata in changes.sql

--- queries/changes.sql
+++ queries/changes.sql
@@ -1,9 +1,10 @@
 # Changes Table
 SELECT 
 source,
-id as change_id,
-time_created,
-event_type 
-FROM four_keys.events_raw 
+event_type,
+JSON_EXTRACT_SCALAR(commit, '$.id') change_id,
+TIMESTAMP_TRUNC(TIMESTAMP(JSON_EXTRACT_SCALAR(commit, '$.timestamp')),second) as time_created,
+FROM four_keys.events_raw e,
+UNNEST(JSON_EXTRACT_ARRAY(e.metadata, '$.commits')) as commit
 WHERE event_type in ("pull_request", "push", "merge_request")
-GROUP BY 1,2,3,4;
+GROUP BY 1,2,3,4
