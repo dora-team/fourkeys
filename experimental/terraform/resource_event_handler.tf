12c4bd001bcf2f6a97fa61d5a3f16bf98e2f3826
Dave Stanke <davidstanke@gmail.com>
2021-06-10 20:09:06 +0000

    ci testing

--- experimental/terraform/resource_event_handler.tf
+++ experimental/terraform/resource_event_handler.tf
@@ -32,15 +32,15 @@ resource "google_cloud_run_service" "event_handler" {
 
 }
 
-# resource "google_cloud_run_service_iam_binding" "noauth" {
-#   location = var.google_region
-#   project  = var.google_project_id
-#   service  = "event-handler"
+resource "google_cloud_run_service_iam_binding" "noauth" {
+  location = var.google_region
+  project  = var.google_project_id
+  service  = "event-handler"
 
-#   role       = "roles/run.invoker"
-#   members    = ["allUsers"]
-#   depends_on = [google_cloud_run_service.event_handler]
-# }
+  role       = "roles/run.invoker"
+  members    = ["allUsers"]
+  depends_on = [google_cloud_run_service.event_handler]
+}
 
 resource "google_secret_manager_secret" "event-handler-secret" {
   secret_id = "event-handler"
