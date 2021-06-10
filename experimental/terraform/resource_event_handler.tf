58d774e2ecc75f33d226d5d7858f092a0f0fd55b
Dave Stanke <davidstanke@gmail.com>
2021-06-10 19:59:03 +0000

    ci testing

--- experimental/terraform/resource_event_handler.tf
+++ experimental/terraform/resource_event_handler.tf
@@ -32,35 +32,35 @@ resource "google_cloud_run_service" "event_handler" {
 
 }
 
-resource "google_cloud_run_service_iam_binding" "noauth" {
-  location = var.google_region
-  project  = var.google_project_id
-  service  = "event-handler"
+# resource "google_cloud_run_service_iam_binding" "noauth" {
+#   location = var.google_region
+#   project  = var.google_project_id
+#   service  = "event-handler"
 
-  role       = "roles/run.invoker"
-  members    = ["allUsers"]
-  depends_on = [google_cloud_run_service.event_handler]
+#   role       = "roles/run.invoker"
+#   members    = ["allUsers"]
+#   depends_on = [google_cloud_run_service.event_handler]
+# }
+
+resource "google_secret_manager_secret" "event-handler-secret" {
+  secret_id = "event-handler"
+  replication {
+    automatic = true
+  }
+  depends_on = [google_project_service.sm_api]
 }
 
-# resource "google_secret_manager_secret" "event-handler-secret" {
-#   secret_id = "event-handler"
-#   replication {
-#     automatic = true
-#   }
-#   depends_on = [google_project_service.sm_api]
-# }
+resource "random_id" "event-handler-random-value" {
+  byte_length = "20"
+}
 
-# resource "random_id" "event-handler-random-value" {
-#   byte_length = "20"
-# }
+resource "google_secret_manager_secret_version" "event-handler-secret-version" {
+  secret      = google_secret_manager_secret.event-handler-secret.id
+  secret_data = random_id.event-handler-random-value.hex
+}
 
-# resource "google_secret_manager_secret_version" "event-handler-secret-version" {
-#   secret      = google_secret_manager_secret.event-handler-secret.id
-#   secret_data = random_id.event-handler-random-value.hex
-# }
-
-# resource "google_secret_manager_secret_iam_member" "event-handler" {
-#   secret_id = google_secret_manager_secret.event-handler-secret.id
-#   role      = "roles/secretmanager.secretAccessor"
-#   member    = "serviceAccount:${google_service_account.fourkeys_service_account.email}"
-# }
+resource "google_secret_manager_secret_iam_member" "event-handler" {
+  secret_id = google_secret_manager_secret.event-handler-secret.id
+  role      = "roles/secretmanager.secretAccessor"
+  member    = "serviceAccount:${google_service_account.fourkeys_service_account.email}"
+}
