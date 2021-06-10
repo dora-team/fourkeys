ccff11f7a2e7b5ac32b3944ab017153df81a574a
Dave Stanke <davidstanke@gmail.com>
2021-06-10 22:49:51 +0000

    disable scheduled queries for now (b/c they don't work in ci)

--- experimental/terraform/resource_bigquery.tf
+++ experimental/terraform/resource_bigquery.tf
@@ -30,23 +30,23 @@ resource "google_bigquery_table" "bq_tables_derived" {
   deletion_protection = false
 }
 
-resource "google_bigquery_data_transfer_config" "scheduled_query" {
+# resource "google_bigquery_data_transfer_config" "scheduled_query" {
 
-  for_each = toset(["changes", "deployments", "incidents"])
+#   for_each = toset(["changes", "deployments", "incidents"])
 
-  display_name           = "four_keys_${each.key}"
-  data_source_id         = "scheduled_query"
-  schedule               = "every 24 hours"
-  destination_dataset_id = google_bigquery_dataset.four_keys.dataset_id
-  location               = var.bigquery_region
-  params = {
-    destination_table_name_template = each.key
-    write_disposition               = "WRITE_TRUNCATE"
-    query                           = file("../../queries/${each.key}.sql")
-  }
-  service_account_name = google_service_account.fourkeys_service_account.email
-  depends_on = [google_project_service.bq_dt_api, google_bigquery_table.bq_table_events_raw, google_bigquery_table.bq_tables_derived]
-}
+#   display_name           = "four_keys_${each.key}"
+#   data_source_id         = "scheduled_query"
+#   schedule               = "every 24 hours"
+#   destination_dataset_id = google_bigquery_dataset.four_keys.dataset_id
+#   location               = var.bigquery_region
+#   params = {
+#     destination_table_name_template = each.key
+#     write_disposition               = "WRITE_TRUNCATE"
+#     query                           = file("../../queries/${each.key}.sql")
+#   }
+#   service_account_name = google_service_account.fourkeys_service_account.email
+#   depends_on = [google_project_service.bq_dt_api, google_bigquery_table.bq_table_events_raw, google_bigquery_table.bq_tables_derived]
+# }
 
 resource "google_project_iam_member" "parser_bq_project_access" {
   role   = "roles/bigquery.user"
