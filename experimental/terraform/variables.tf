5f64bd93ed2510f290e2cb3e8480566a5bf145ac
Dave Stanke <davidstanke@gmail.com>
2021-06-10 19:08:41 +0000

    testing tf e2e

--- experimental/terraform/variables.tf
+++ experimental/terraform/variables.tf
@@ -6,17 +6,17 @@ variable "google_region" {
   type = string
 }
 
-variable "bigquery_region" {
-  type = string
-  validation {
-    condition = (
-      contains(["US", "EU"], var.bigquery_region)
-    )
-    error_message = "The value for 'bigquery_region' must be one of: 'US','EU'."
-  }
-}
+# variable "bigquery_region" {
+#   type = string
+#   validation {
+#     condition = (
+#       contains(["US", "EU"], var.bigquery_region)
+#     )
+#     error_message = "The value for 'bigquery_region' must be one of: 'US','EU'."
+#   }
+# }
 
-variable "parsers" {
-  type        = list(any)
-  description = "list of data parsers to configure (e.g. 'gitlab','tekton')"
-}
+# variable "parsers" {
+#   type        = list(any)
+#   description = "list of data parsers to configure (e.g. 'gitlab','tekton')"
+# }
