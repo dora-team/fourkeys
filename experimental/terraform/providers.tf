bbf69c750377c819a6c743b6a34449fd0bf0142c
Matthew Green <matthewgr@nandos.co.uk>
2021-07-30 11:32:18 +0100

    Add terraform SA impersonation for cloud build execution

--- experimental/terraform/providers.tf
+++ experimental/terraform/providers.tf
@@ -1,4 +1,37 @@
+# Copyright 2020 Google LLC
+# Copyright 2021 Nando’s Chickenland Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 provider "google" {
-  project = var.google_project_id
-  region  = var.google_region
+  alias = "token"
+}
+
+data "google_service_account_access_token" "default" {
+  provider               = google.token
+  target_service_account = "terraform@${var.google_project_id}.iam.gserviceaccount.com"
+  scopes                 = ["cloud-platform"]
+  lifetime               = "3600s"
+}
+
+provider "google" {
+  access_token = data.google_service_account_access_token.default.access_token
+  project      = var.google_project_id
+  region       = var.google_region
+}
+
+provider "google-beta" {
+  access_token = data.google_service_account_access_token.default.access_token
+  project      = var.google_project_id
+  region       = var.google_region
 }
