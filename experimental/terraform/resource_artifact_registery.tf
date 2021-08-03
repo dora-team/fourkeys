7f600471df1f9e0d515c9e0b34e5855ae05eb902
faizando <faizanj@nandos.co.uk>
2021-08-03 11:25:21 +0100

    NAP-275 create artifact registry

---
+++ experimental/terraform/resource_artifact_registery.tf
@@ -0,0 +1,17 @@
+resource "google_artifact_registry_repository" "default" {
+  provider = google-beta
+
+  description   = "Container Repository"
+  format        = "DOCKER"
+  location      = var.google_region
+  project       = var.google_project_id
+  repository_id = "default"
+}
+
+module "pubsub" {
+  source  = "terraform-google-modules/pubsub/google"
+  version = "~> 1.8"
+
+  topic      = "gcr" # see https://cloud.google.com/artifact-registry/docs/configure-notifications for set up and consumption
+  project_id = var.google_project_id
+}
