e338a6d6ae32c82b9e36cbd37e5b92992c81b56a
Faizan J <49913297+faizando@users.noreply.github.com>
2021-08-10 11:04:16 +0100

    NAP-275 resolve data parser var error

--- experimental/terraform/resource_parsers.tf
+++ experimental/terraform/resource_parsers.tf
@@ -2,7 +2,7 @@ module "data_parser_service" {
   for_each                       = toset(var.parsers)
   source                         = "./data_parser"
 
-  branch                         = var.cloud_build_branch
+  cloud_build_branch             = var.cloud_build_branch
   fourkeys_service_account_email = google_service_account.fourkeys.email
   google_project_id              = var.google_project_id
   google_region                  = var.google_region
