4643757e9d285676a36936586cfc16a51e64e79b
Faizan J <49913297+faizando@users.noreply.github.com>
2021-08-10 10:58:21 +0100

    NAP-275 remove un used pubsub module

--- experimental/terraform/resource_artifact_registery.tf
+++ experimental/terraform/resource_artifact_registery.tf
@@ -15,11 +15,3 @@ resource "google_artifact_registry_repository" "default" {
     google_project_service.ar_api,
   ]
 }
-
-module "pubsub" {
-  source  = "terraform-google-modules/pubsub/google"
-  version = "~> 1.8"
-
-  topic      = "gcr" # see https://cloud.google.com/artifact-registry/docs/configure-notifications for set up and consumption
-  project_id = var.google_project_id
-}
