dae7aa3ea9a703d3e5102de0c32cec3fe8eebbf3
Faizan J <49913297+faizando@users.noreply.github.com>
2021-08-12 13:20:46 +0100

    NAP-275 use func to ouput  domain mapping resource

--- experimental/terraform/outputs.tf
+++ experimental/terraform/outputs.tf
@@ -3,7 +3,7 @@ output "event_handler_endpoint" {
 }
 
 output "event_handler_dns" {
-  value = try(google_cloud_run_domain_mapping.event_handler[0].id, null)
+  value = try(google_cloud_run_domain_mapping.event_handler[0].status[0]["resource_records"], null)
 }
 
 output "event_handler_secret" {
