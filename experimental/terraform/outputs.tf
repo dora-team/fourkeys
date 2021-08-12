8b615fee32632bfd79d217332f3cc6daf3e4df70
Faizan J <49913297+faizando@users.noreply.github.com>
2021-08-12 11:27:59 +0100

    NAP-275 use conditional operator to ouput  domain mapping resource

--- experimental/terraform/outputs.tf
+++ experimental/terraform/outputs.tf
@@ -3,7 +3,7 @@ output "event_handler_endpoint" {
 }
 
 output "event_handler_dns" {
-  value = google_cloud_run_domain_mapping.event_handler[0] > 0 ? google_cloud_run_domain_mapping.event_handler[0].status[0].resource_records : "na"
+  value = google_cloud_run_domain_mapping.event_handler[0].status[0].resource_records > 0 ? google_cloud_run_domain_mapping.event_handler[0].status[0].resource_records : "na"
 }
 
 output "event_handler_secret" {
