52171d6d87dba46690911dcc1835dd97bf8a8c8a
Faizan J <49913297+faizando@users.noreply.github.com>
2021-08-12 13:09:06 +0100

    NAP-275 WIP test output

--- experimental/terraform/outputs.tf
+++ experimental/terraform/outputs.tf
@@ -3,7 +3,7 @@ output "event_handler_endpoint" {
 }
 
 output "event_handler_dns" {
-  value = length(var.google_domain_mapping_region) > 0 ? google_cloud_run_domain_mapping.event_handler[0].status[0].resource_records : "na"
+  value = length(var.google_domain_mapping_region) > 0 ? "test" : "na"
 }
 
 output "event_handler_secret" {
