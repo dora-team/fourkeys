ffd3b5d2e8bdab440872a25916584eed406fdded
Faizan J <49913297+faizando@users.noreply.github.com>
2021-08-13 10:42:08 +0100

    NAP-275 domain mapping

--- experimental/terraform/outputs.tf
+++ experimental/terraform/outputs.tf
@@ -3,7 +3,7 @@ output "event_handler_endpoint" {
 }
 
 output "event_handler_dns" {
-  value = try(google_cloud_run_domain_mapping.event_handler[0].status[0]["resource_records"], null)
+  value = try(google_cloud_run_domain_mapping.event_handler[0].status[0]["resource_records"][0], null)
 }
 
 output "event_handler_secret" {
