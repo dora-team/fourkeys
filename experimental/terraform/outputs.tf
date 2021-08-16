2a27498be44a3de5ac33bf2bec953a5680022073
Faizan J <49913297+faizando@users.noreply.github.com>
2021-08-16 11:42:55 +0100

    NAP-275 update outputs
    Co-authored-by: Matthew Green <matthewgr@nandos.co.uk>
    Co-authored-by: Marco Hernandez <marcoths@users.noreply.github.com>

--- experimental/terraform/outputs.tf
+++ experimental/terraform/outputs.tf
@@ -2,9 +2,8 @@ output "event_handler_endpoint" {
   value = google_cloud_run_service.event_handler.status[0]["url"]
 }
 
-output "event_handler_dns" {
-  # value = try(google_cloud_run_domain_mapping.event_handler[0].status[0]["resource_records"][0], null)
-  value = try(module.event_hander_dns.name_servers, null)
+output "event_handler_name_servers" {
+  value = try(module.event_hander_dns[0].name_servers, null)
 }
 
 output "event_handler_secret" {
