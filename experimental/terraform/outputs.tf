206553f282ec385d6d063393272526421227b877
Faizan J <49913297+faizando@users.noreply.github.com>
2021-08-17 09:54:47 +0100

    NAP-275 add event handler outputs

--- experimental/terraform/outputs.tf
+++ experimental/terraform/outputs.tf
@@ -2,6 +2,18 @@ output "event_handler_endpoint" {
   value = google_cloud_run_service.event_handler.status[0]["url"]
 }
 
+output "event_handler_dns_data" {
+  value = try(google_cloud_run_domain_mapping.event_handler[0].status[0]["resource_records"][0].data, null)
+}
+
+output "event_handler_dns_name" {
+  value = try(google_cloud_run_domain_mapping.event_handler[0].status[0]["resource_records"][0].name, null)
+}
+
+output "event_handler_dns_type" {
+  value = try(google_cloud_run_domain_mapping.event_handler[0].status[0]["resource_records"][0].type, null)
+}
+
 output "event_handler_name_servers" {
   value = try(module.event_hander_dns[0].name_servers, null)
 }
