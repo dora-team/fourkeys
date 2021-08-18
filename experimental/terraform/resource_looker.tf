7ce3172be1153ce29c8c65b96f6ec7d33815edee
Faizan J <49913297+faizando@users.noreply.github.com>
2021-08-18 11:03:51 +0100

    NAp-275 make looker config conditional
    Co-authored-by: Marco Hernandez <marcoths@users.noreply.github.com>
    Co-authored-by: Matthew Green <matthewgr@nandos.co.uk>

--- experimental/terraform/resource_looker.tf
+++ experimental/terraform/resource_looker.tf
@@ -1,6 +1,7 @@
 module "service_account_for_looker" {
   source        = "terraform-google-modules/service-accounts/google"
   version       = "~> 3.0"
+  count          = var.looker_service_account ? 1 : 0
 
   display_name  = "Looker"
   description   = "Looker accessor account (managed by Terraform)"
@@ -12,6 +13,8 @@ module "service_account_for_looker" {
 }
 
 resource "google_project_organization_policy" "service_account_keys_policy" {
+  count      = var.service_account_keys_policy_override ? 1 : 0
+
   project    = var.google_project_id
   constraint = "iam.disableServiceAccountKeyCreation"
 
