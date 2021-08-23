9231727c6a5e46d10184e3ff1ccee48c18d56a12
Matthew Green <matthewgr@nandos.co.uk>
2021-08-23 17:32:11 +0100

    Add cloud build trigger outputs for notifications

---
+++ experimental/terraform/cloudbuild-trigger/outputs.tf
@@ -0,0 +1,7 @@
+output "id" {
+  value = google_cloudbuild_trigger.continuous_provisioning_trigger.trigger_id
+}
+
+output "name" {
+  value = google_cloudbuild_trigger.continuous_provisioning_trigger.name
+}
