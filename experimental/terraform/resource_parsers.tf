e8b75194871b064076782624989a4cfbf6e9706f
Matthew Green <matthewgr@nandos.co.uk>
2021-08-23 17:34:36 +0100

    Add data_parser(s) cloud build notification(s)

--- experimental/terraform/resource_parsers.tf
+++ experimental/terraform/resource_parsers.tf
@@ -1,14 +1,17 @@
 module "data_parser_service" {
-  for_each                       = toset(var.parsers)
-  source                         = "./data_parser"
+  for_each                        = toset(var.parsers)
+  source                          = "./data_parser"
 
-  cloud_build_branch             = var.cloud_build_branch
-  fourkeys_service_account_email = google_service_account.fourkeys.email
-  google_project_id              = var.google_project_id
-  google_region                  = var.google_region
-  owner                          = var.owner
-  parser_service_name            = each.key
-  repository                     = var.repository
+  cloud_build_branch              = var.cloud_build_branch
+  cloud_run_service_account_email = module.service_account_for_cloudrun.email
+  fourkeys_service_account_email  = google_service_account.fourkeys.email
+  google_project_id               = var.google_project_id
+  google_region                   = var.google_region
+  owner                           = var.owner
+  notification_url                = length(var.mapped_domain) > 0 ? try("https://${var.subdomain}.${var.mapped_domain}", null) : google_cloud_run_service.event_handler.status[0]["url"]
+  parser_service_name             = each.key
+  repository                      = var.repository
+  storage_bucket                  = module.bucket_for_cloudrun.bucket
 
   depends_on = [
     google_project_service.run_api
