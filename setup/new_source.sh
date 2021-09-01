d1b5f55661ad44b13f483c03d0df3d20aafdd3e7
Dave Stanke <davidstanke@gmail.com>
2021-09-01 22:31:43 +0000

    update README - add migration instructions

--- setup/new_source.sh
+++ setup/new_source.sh
@@ -1,1 +1,1 @@
-### TODO: we need to make a new version of new_source.sh (see old version in "deprecated") for compatibility with the terraform installer
+### TODO: #195 we need to make a new version of new_source.sh (see old version in "deprecated") for compatibility with the terraform installer
