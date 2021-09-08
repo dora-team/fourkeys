fe759424eca2d74f8d94fd8c0d13f5ffc3ba92f4
Dina Graves Portman <dinagraves@google.com>
2021-09-07 18:29:05 -0700

    Removing env

--- setup/env.sh
+++
@@ -1,7 +0,0 @@
-export FOURKEYS_PROJECT=fourkeys-025822
-export FOURKEYS_REGION=us-central1
-export HELLOWORLD_PROJECT=helloworld-025822
-export HELLOWORLD_ZONE=us-central1-a
-export BILLING_ACCOUNT=billingAccounts/004C5F-81440F-54DEBB
-export PARENT_PROJECT=broadest-ff
-export PARENT_FOLDER=
