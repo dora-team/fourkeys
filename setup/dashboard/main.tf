816ff26cd69fa460c93cff7e16fd2134c8765c76
Dina Graves Portman <dinagraves@google.com>
2021-09-07 18:48:41 -0700

    Fixing copy pasta error

--- setup/dashboard/main.tf
+++ setup/dashboard/main.tf
@@ -31,5 +31,5 @@ resource "google_cloud_run_service_iam_binding" "noauth" {
 
   role       = "roles/run.invoker"
   members    = ["allUsers"]
-  depends_on = [google_cloud_run_service.event_handler]
+  depends_on = [google_cloud_run_service.dashboard]
 }
