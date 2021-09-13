b4d41ef58c68b03f626bde593b1041f72dbda6f0
Faizan J <49913297+faizando@users.noreply.github.com>
2021-09-13 11:18:28 +0100

    NAP-313 add webhook to list of repo

--- experimental/github-webhook/program.py
+++ experimental/github-webhook/program.py
@@ -12,23 +12,23 @@ import click
 def cli(githubtoken, owner, repo, webhookurl, webhooksecret):
     """Simple program that adds a webhook to github repo."""
     addHook(githubtoken, owner, repo, webhookurl, webhooksecret)
-        
+    
 def addHook(githubtoken, owner, repo, webhookurl, webhooksecret):
     g = Github(githubtoken)
-    repo = g.get_repo(f'{owner}/{repo}')
-
-    EVENTS = ["*"]
-    WEBHOOK_URL=f'{webhookurl}'  
-    config = {
-        "url": "{webhook_url}".format(webhook_url=WEBHOOK_URL),
-        "content_type": "json"
-    }
-    if len(webhooksecret) >0:
-        config["secret"]=webhooksecret
-    try:
-        repo.create_hook("web", config, EVENTS, active=True)
-    except:
-        pass # hook already exists
+    for repo in g.get_user().get_repos():
+        print(repo.name)
+        EVENTS = ["*"]
+        WEBHOOK_URL=f'{webhookurl}'  
+        config = {
+            "url": "{webhook_url}".format(webhook_url=WEBHOOK_URL),
+            "content_type": "json"
+        }
+        if len(webhooksecret) >0:
+            config["secret"]=webhooksecret
+        # try:
+        #     repo.create_hook("web", config, EVENTS, active=True)
+        # except:
+        #     pass # hook already exists
 
 if __name__ == '__main__':
     cli()
