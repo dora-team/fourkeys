eb8f9dc9bfef8ae794e58e2b4c347f1f32a9a4b7
Roger Martinez <roger.that.dev@gmail.com>
2021-09-14 21:44:05 -0700

    more data source module

---
+++ experimental/terraform/modules/fourkeys-data-source/variables.tf
@@ -0,0 +1,15 @@
+variable "parser_service_name" {
+  type = string
+}
+
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "fourkeys_service_account_email" {
+  type = string
+}
