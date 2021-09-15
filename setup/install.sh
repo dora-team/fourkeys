288b6656fa1601dea932d4a228dd432ce24d46e0
Dina Graves Portman <dinagraves@google.com>
2021-09-15 09:56:40 -0700

    Update URL link
    Co-authored-by: Dave Stanke <davidstanke@google.com>

--- setup/install.sh
+++ setup/install.sh
@@ -91,7 +91,7 @@ fi
 
 echo "••••••••🔑••🔑••🔑••🔑••••••••"
 echo "configuring Grafana dashboard…"
-DASHBOARD_URL="$(terraform output -raw dashboard_endpoint)d/yVtwoQ4nk/four-keys?orgId=1"
+DASHBOARD_URL="$(terraform output -raw dashboard_endpoint)/d/yVtwoQ4nk/four-keys?orgId=1"
 
 echo -e "Please visit ${GREEN}$DASHBOARD_URL${NOCOLOR} to view your data in the dashboard template."
 
