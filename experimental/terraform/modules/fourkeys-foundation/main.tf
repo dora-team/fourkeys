23e8dad9908406331a5f2ab6cce6658afa65292b
Roger Martinez <roger.that.dev@gmail.com>
2021-09-15 16:43:18 -0700

    add run service for event handler in foundation

--- experimental/terraform/modules/fourkeys-foundation/main.tf
+++ experimental/terraform/modules/fourkeys-foundation/main.tf
@@ -9,7 +9,8 @@ locals {
     "cloudapis.googleapis.com",
     "run.googleapis.com",
     "cloudbuild.googleapis.com",
-    "containerregistry.googleapis.com"
+    "containerregistry.googleapis.com",
+    "secretmanager.googleapis.com"
 
   ])
 }
@@ -55,3 +56,67 @@ resource "google_project_service" "all" {
   service                    = each.value
   disable_dependent_services = true
 }
+
+resource "google_cloud_run_service" "event_handler" {
+  name     = "event-handler"
+  project  = var.project_id
+  location = var.region
+
+  template {
+    spec {
+      containers {
+        image = "gcr.io/${var.project_id}/event-handler"
+        env {
+          name  = "PROJECT_NAME"
+          value = var.project_id
+        }
+      }
+      service_account_name = google_service_account.fourkeys.email
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+
+  autogenerate_revision_name = true
+
+  depends_on = [
+    google_project_service.all["run.googleapis.com"]
+  ]
+
+}
+
+# resource "google_cloud_run_service_iam_binding" "noauth" {
+#   location = var.google_region
+#   project  = var.google_project_id
+#   service  = "event-handler"
+
+#   role       = "roles/run.invoker"
+#   members    = ["allUsers"]
+#   depends_on = [google_cloud_run_service.event_handler]
+# }
+
+# resource "google_secret_manager_secret" "event_handler" {
+#   secret_id = "event-handler"
+#   replication {
+#     automatic = true
+#   }
+#   depends_on = [google_project_service.sm_api]
+# }
+
+# resource "random_id" "event_handler_random_value" {
+#   byte_length = "20"
+# }
+
+# resource "google_secret_manager_secret_version" "event_handler" {
+#   secret      = google_secret_manager_secret.event_handler.id
+#   secret_data = random_id.event_handler_random_value.hex
+# }
+
+# resource "google_secret_manager_secret_iam_member" "event_handler" {
+#   secret_id = google_secret_manager_secret.event_handler.id
+#   role      = "roles/secretmanager.secretAccessor"
+#   member    = "serviceAccount:${google_service_account.fourkeys.email}"
+# }
