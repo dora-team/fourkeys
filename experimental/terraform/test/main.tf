c9cb8bfc8d2b8ed6f9abb2930c8c9c766bbaa9e6
Roger Martinez <roger.that.dev@gmail.com>
2021-09-14 18:32:00 -0700

    test dir in terraform experimental

---
+++ experimental/terraform/test/main.tf
@@ -0,0 +1,12 @@
+module "foundation" {
+  source     = "../modules/fourkeys-foundation"
+  project_id = "fourkeytest"
+}
+
+module "bigquery" {
+  source                   = "../modules/fourkeys-bigquery"
+  project_id               = "fourkeytest"
+  bigquery_region          = "us-central1"
+  fourkeys_service_account = module.foundation.fourkeys_service_account_email
+}
+
