cef7831cf30bc993cd01bb4b6bffbdb624a36cf9
Roger Martinez <roger.that.dev@gmail.com>
2021-09-28 12:50:22 -0700

    formatting

--- experimental/terraform/modules/fourkeys-data-source/main.tf
+++ experimental/terraform/modules/fourkeys-data-source/main.tf
@@ -3,16 +3,14 @@ data "google_project" "project" {
 }
 
 module "gcloud_build_data_source" {
-  source  = "terraform-google-modules/gcloud/google"
-  version = "~> 2.0"
-
-  platform              = "linux"
-  additional_components = []
-
+  source                 = "terraform-google-modules/gcloud/google"
+  version                = "~> 2.0"
+  platform               = "linux"
+  additional_components  = []
   create_cmd_entrypoint  = "gcloud"
   create_cmd_body        = "builds submit ${path.module}/files/bq-workers/${var.parser_service_name}-parser --tag=gcr.io/${var.project_id}/${var.parser_service_name}-parser --project=${var.project_id}"
   destroy_cmd_entrypoint = "gcloud"
-  destroy_cmd_body       = "container images delete gcr.io/${var.project_id}}/${var.parser_service_name}-parser --quiet"
+  destroy_cmd_body       = "container images delete gcr.io/${var.project_id}/${var.parser_service_name}-parser --quiet"
 }
 
 resource "google_cloud_run_service" "parser" {
