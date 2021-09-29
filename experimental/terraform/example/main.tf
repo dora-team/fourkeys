1dd4d1b6d05a2e492ba2e0fc978d360e511bc0ce
Roger Martinez <roger.that.dev@gmail.com>
2021-09-29 09:45:48 -0700

    example files

--- experimental/terraform/example/main.tf
+++ experimental/terraform/example/main.tf
@@ -1,15 +1,32 @@
+terraform {
+  required_version = ">= 0.15"
+  required_providers {
+    google = {
+      version = "~> 3.86.0"
+    }
+  }
+}
+
 module "foundation" {
   source     = "../modules/fourkeys-foundation"
-  project_id = "fourkeytest"
+  project_id = var.project_id
 }
 
 module "bigquery" {
   source                   = "../modules/fourkeys-bigquery"
-  project_id               = "fourkeytest"
-  bigquery_region          = "us-central1"
-  fourkeys_service_account = module.foundation.fourkeys_service_account_email
+  project_id               = var.project_id
+  bigquery_region          = var.region
+  fourkeys_service_account_email = module.foundation.fourkeys_service_account_email
   depends_on = [
     module.foundation
   ]
-  parser_service_name = "github"
+}
+
+module "github_parser" {
+  source                   = "../modules/fourkeys-data-source"
+  for_each  = toset(var.parsers)
+  project_id               = var.project_id
+  parser_service_name          = each.value
+  region  = var.region
+  fourkeys_service_account_email = module.foundation.fourkeys_service_account_email
 }
