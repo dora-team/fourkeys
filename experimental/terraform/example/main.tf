efbd57aa86855f383f29a9e0e2a82ea352275887
Roger Martinez <roger.that.dev@gmail.com>
2021-10-20 16:21:15 -0700

    update example sources to github URLs

--- experimental/terraform/example/main.tf
+++ experimental/terraform/example/main.tf
@@ -8,12 +8,20 @@ terraform {
 }
 
 module "foundation" {
-  source     = "../modules/fourkeys-foundation"
+  source     = "github.com/GoogleCloudPlatform/fourkeys//experimental/terraform/modules/fourkeys-foundation"
   project_id = var.project_id
+}
+output "event_handler_endpoint" {
+  value = module.foundation.event_handler_endpoint
+}
+
+output "event_handler_secret" {
+  value = module.foundation.event_handler_secret
+  sensitive = true
 }
 
 module "bigquery" {
-  source                   = "../modules/fourkeys-bigquery"
+  source                   = "github.com/GoogleCloudPlatform/fourkeys//experimental/terraform/modules/fourkeys-bigquery"
   project_id               = var.project_id
   bigquery_region          = var.region
   fourkeys_service_account_email = module.foundation.fourkeys_service_account_email
@@ -23,7 +31,7 @@ module "bigquery" {
 }
 
 module "github_parser" {
-  source                   = "../modules/fourkeys-data-source"
+  source                   = "github.com/GoogleCloudPlatform/fourkeys//experimental/terraform/modules/fourkeys-data-source"
   for_each  = toset(var.parsers)
   project_id               = var.project_id
   parser_service_name          = each.value
