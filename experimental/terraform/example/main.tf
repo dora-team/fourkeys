a1403dcd17932910d2d002dde40bad89a27114ea
Roger Martinez <roger.that.dev@gmail.com>
2022-01-25 13:15:12 -0800

    remove dependency

--- experimental/terraform/example/main.tf
+++ experimental/terraform/example/main.tf
@@ -48,7 +48,4 @@ module "dashboard" {
   project_id                     = var.project_id
   region                         = var.region
   fourkeys_service_account_email = module.foundation.fourkeys_service_account_email
-  depends_on = [
-    module.foundation
-  ]
 }
