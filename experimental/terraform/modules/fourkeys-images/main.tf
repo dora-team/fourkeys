e45ec7c250b9a0e85c918f6bb3a9937297f3310d
rogerthatdev <roger.that.dev@gmail.com>
2022-03-14 15:13:49 -0700

    add service lines to images module

--- experimental/terraform/modules/fourkeys-images/main.tf
+++ experimental/terraform/modules/fourkeys-images/main.tf
@@ -1,3 +1,15 @@
+locals {
+    services = var.enable_apis ? [
+    "cloudbuild.googleapis.com"
+  ] : []
+}
+
+resource "google_project_service" "images_services" {
+  for_each                   = toset(local.services)
+  service                    = each.value
+  disable_on_destroy         = false
+}
+
 module "gcloud_build_dashboard" {
   source                 = "terraform-google-modules/gcloud/google"
   version                = "~> 2.0"
@@ -7,6 +19,9 @@ module "gcloud_build_dashboard" {
   create_cmd_body        = "builds submit ${path.module}/files/dashboard --tag=gcr.io/${var.project_id}/fourkeys-grafana-dashboard --project=${var.project_id}"
   destroy_cmd_entrypoint = "gcloud"
   destroy_cmd_body       = "container images delete gcr.io/${var.project_id}/fourkeys-grafana-dashboard --quiet"
+  depends_on = [
+    google_project_service.images_services
+  ]
 }
 
 module "gcloud_build_data_source" {
@@ -19,6 +34,9 @@ module "gcloud_build_data_source" {
   create_cmd_body        = "builds submit ${path.module}/files/bq-workers/${each.value}-parser --tag=gcr.io/${var.project_id}/${each.value}-parser --project=${var.project_id}"
   destroy_cmd_entrypoint = "gcloud"
   destroy_cmd_body       = "container images delete gcr.io/${var.project_id}/${each.value}-parser --quiet"
+  depends_on = [
+    google_project_service.images_services
+  ]
 }
 
 module "gcloud_build_event_handler" {
@@ -28,4 +46,7 @@ module "gcloud_build_event_handler" {
   create_cmd_body        = "builds submit ${path.module}/files/event_handler --tag=gcr.io/${var.project_id}/event-handler --project=${var.project_id}"
   destroy_cmd_entrypoint = "gcloud"
   destroy_cmd_body       = "container images delete gcr.io/${var.project_id}/event-handler --quiet"
+  depends_on = [
+    google_project_service.images_services
+  ]
 }
