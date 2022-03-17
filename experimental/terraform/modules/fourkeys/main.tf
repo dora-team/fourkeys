34bd09c327b701859eb0e83f7a507d263a8eed77
rogerthatdev <roger.that.dev@gmail.com>
2022-03-17 08:41:52 -0700

    formatting

--- experimental/terraform/modules/fourkeys/main.tf
+++ experimental/terraform/modules/fourkeys/main.tf
@@ -1,11 +1,11 @@
 locals {
-  event_handler_container_url = var.enable_build_images ? "gcr.io/${var.project_id}/event-handler" : var.event_handler_container_url
-  dashboard_container_url     = var.enable_build_images ? "gcr.io/${var.project_id}/fourkeys-grafana-dashboard" : var.dashboard_container_url
+  event_handler_container_url = var.enable_build_images ? format("gcr.io/%s/event-handler", var.project_id) : var.event_handler_container_url
+  dashboard_container_url     = var.enable_build_images ? format("gcr.io/%s/fourkeys-grafana-dashboard", var.project_id) : var.dashboard_container_url
   parser_container_urls = var.enable_build_images ? {
-    "github" = "gcr.io/${var.project_id}/github-parser",
-    "gitlab" = "gcr.io/${var.project_id}/gitlab-parser",
-    "cloud-build" = "gcr.io/${var.project_id}/cloud-build-parser",
-    "tekton" = "gcr.io/${var.project_id}/tekton-parser",
+    "github" = format("gcr.io/%s/github-parser", var.project_id)
+    "gitlab" = format("gcr.io/%s/gitlab-parser", var.project_id)
+    "cloud-build" = format("gcr.io/%s/cloud-build-parser", var.project_id)
+    "tekton" = format("gcr.io/%s/tekton-parser", var.project_id)
   } : var.parser_container_urls
 }
 
