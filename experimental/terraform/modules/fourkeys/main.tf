1e5c898deb165eb82e711dec7ba29dcfdbbb3537
rogerthatdev <roger.that.dev@gmail.com>
2022-03-17 08:42:26 -0700

    more formatting

--- experimental/terraform/modules/fourkeys/main.tf
+++ experimental/terraform/modules/fourkeys/main.tf
@@ -2,10 +2,10 @@ locals {
   event_handler_container_url = var.enable_build_images ? format("gcr.io/%s/event-handler", var.project_id) : var.event_handler_container_url
   dashboard_container_url     = var.enable_build_images ? format("gcr.io/%s/fourkeys-grafana-dashboard", var.project_id) : var.dashboard_container_url
   parser_container_urls = var.enable_build_images ? {
-    "github" = format("gcr.io/%s/github-parser", var.project_id)
-    "gitlab" = format("gcr.io/%s/gitlab-parser", var.project_id)
+    "github"      = format("gcr.io/%s/github-parser", var.project_id)
+    "gitlab"      = format("gcr.io/%s/gitlab-parser", var.project_id)
     "cloud-build" = format("gcr.io/%s/cloud-build-parser", var.project_id)
-    "tekton" = format("gcr.io/%s/tekton-parser", var.project_id)
+    "tekton"      = format("gcr.io/%s/tekton-parser", var.project_id)
   } : var.parser_container_urls
 }
 
@@ -18,10 +18,10 @@ module "fourkeys_images" {
 }
 
 module "foundation" {
-  source      = "../fourkeys-foundation"
-  project_id  = var.project_id
+  source                      = "../fourkeys-foundation"
+  project_id                  = var.project_id
   event_handler_container_url = local.event_handler_container_url
-  enable_apis = var.enable_apis
+  enable_apis                 = var.enable_apis
   depends_on = [
     module.fourkeys_images
   ]
