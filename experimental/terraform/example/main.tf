8036aadd90fc92747986f27a5fb1d157176a8579
rogerthatdev <roger.that.dev@gmail.com>
2022-04-08 02:09:13 -0700

    enable_apis true to test with fresh project

--- experimental/terraform/example/main.tf
+++ experimental/terraform/example/main.tf
@@ -10,6 +10,7 @@ terraform {
 module "fourkeys" {
   source = "../modules/fourkeys"
   project_id = var.project_id
+  enable_apis = true
   region = var.region
   bigquery_region = var.bigquery_region
   parsers = var.parsers
