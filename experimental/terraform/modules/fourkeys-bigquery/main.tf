b50b8d6c0278a2a144270309dc28d8f3a21e3731
rogerthatdev <roger.that.dev@gmail.com>
2022-04-08 01:54:56 -0700

    add project to service resources

--- experimental/terraform/modules/fourkeys-bigquery/main.tf
+++ experimental/terraform/modules/fourkeys-bigquery/main.tf
@@ -5,6 +5,7 @@ locals {
 }
 
 resource "google_project_service" "bigquery_services" {
+  project                    = var.project_id
   for_each                   = toset(local.services)
   service                    = each.value
   disable_on_destroy         = false
