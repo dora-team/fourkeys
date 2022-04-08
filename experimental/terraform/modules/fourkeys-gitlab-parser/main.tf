832cac10c6884e84d3a581385e9c1d56fa4fb6b1
rogerthatdev <roger.that.dev@gmail.com>
2022-04-08 01:55:32 -0700

    move iam binding back to parser modules

--- experimental/terraform/modules/fourkeys-gitlab-parser/main.tf
+++ experimental/terraform/modules/fourkeys-gitlab-parser/main.tf
@@ -69,3 +69,9 @@ resource "google_pubsub_subscription" "gitlab" {
     }
   }
 }
+
+resource "google_project_iam_member" "pubsub_service_account_token_creator" {
+  project = var.project_id
+  member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+  role    = "roles/iam.serviceAccountTokenCreator"
+}
