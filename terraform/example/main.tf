7dc5a794f5c641d7652ea7f4b2446ec45264f3aa
Roger Martinez <rogerthatdev@gmail.com>
2022-05-24 15:42:16 -0700

    refactor: rm terraform block from example main tf

--- terraform/example/main.tf
+++ terraform/example/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_version = ">= 0.15"
-  required_providers {
-    google = {
-      version = "~> 3.86.0"
-    }
-  }
-}
-
 module "fourkeys" {
   source              = "github.com/GoogleCloudPlatform/fourkeys//terraform/modules/fourkeys"
   project_id          = var.project_id
