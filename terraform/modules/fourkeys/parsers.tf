6eeb17aa5c49556ef7881b2334b4d4a40e53a3d2
Roger Martinez <rogerthatdev@gmail.com>
2022-08-12 18:14:53 -0700

    refactor locals to use new vars

--- terraform/modules/fourkeys/parsers.tf
+++ terraform/modules/fourkeys/parsers.tf
@@ -2,7 +2,7 @@ module "circleci_parser" {
   source                         = "../fourkeys-circleci-parser"
   count                          = contains(var.parsers, "circleci") ? 1 : 0
   project_id                     = var.project_id
-  parser_container_url           = local.parser_container_urls["circleci"]
+  parser_container_url           = local.circleci_parser_url
   region                         = var.region
   fourkeys_service_account_email = google_service_account.fourkeys.email
   enable_apis                    = var.enable_apis
@@ -15,7 +15,7 @@ module "github_parser" {
   source                         = "../fourkeys-github-parser"
   count                          = contains(var.parsers, "github") ? 1 : 0
   project_id                     = var.project_id
-  parser_container_url           = local.parser_container_urls["github"]
+  parser_container_url           = local.github_parser_url
   region                         = var.region
   fourkeys_service_account_email = google_service_account.fourkeys.email
   enable_apis                    = var.enable_apis
@@ -28,7 +28,7 @@ module "gitlab_parser" {
   source                         = "../fourkeys-gitlab-parser"
   count                          = contains(var.parsers, "gitlab") ? 1 : 0
   project_id                     = var.project_id
-  parser_container_url           = local.parser_container_urls["gitlab"]
+  parser_container_url           = local.gitlab_parser_url
   region                         = var.region
   fourkeys_service_account_email = google_service_account.fourkeys.email
   enable_apis                    = var.enable_apis
@@ -41,7 +41,7 @@ module "pagerduty_parser" {
   source                         = "../fourkeys-pagerduty-parser"
   count                          = contains(var.parsers, "pagerduty") ? 1 : 0
   project_id                     = var.project_id
-  parser_container_url           = local.parser_container_urls["pagerduty"]
+  parser_container_url           = local.pagerduty_parser_url
   region                         = var.region
   fourkeys_service_account_email = google_service_account.fourkeys.email
   enable_apis                    = var.enable_apis
@@ -54,7 +54,7 @@ module "tekton_parser" {
   source                         = "../fourkeys-tekton-parser"
   count                          = contains(var.parsers, "tekton") ? 1 : 0
   project_id                     = var.project_id
-  parser_container_url           = local.parser_container_urls["tekton"]
+  parser_container_url           = local.tekton_parser_url
   region                         = var.region
   fourkeys_service_account_email = google_service_account.fourkeys.email
   enable_apis                    = var.enable_apis
@@ -67,7 +67,7 @@ module "cloud_build_parser" {
   source                         = "../fourkeys-cloud-build-parser"
   count                          = contains(var.parsers, "cloud-build") ? 1 : 0
   project_id                     = var.project_id
-  parser_container_url           = local.parser_container_urls["cloud-build"]
+  parser_container_url           = local.cloud_build_parser_url
   region                         = var.region
   fourkeys_service_account_email = google_service_account.fourkeys.email
   enable_apis                    = var.enable_apis
