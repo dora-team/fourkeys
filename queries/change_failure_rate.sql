850542121dca74b2a12ada9bcbe40f7f6c502334
Simen Seeberg-Rommetveit <simensee@gmail.com>
2023-07-27 14:36:05 +0200

    change failure rate with jira bugs

--- queries/change_failure_rate.sql
+++ queries/change_failure_rate.sql
@@ -1,13 +1,26 @@
-SELECT
-TIMESTAMP_TRUNC(d.time_created, DAY) as day,
-COUNT(DISTINCT IF(i.incident_id is NULL, NULL, deploy_id)) / COUNT(DISTINCT deploy_id) as change_fail_rate
-FROM four_keys.deployments d, d.changes
-LEFT JOIN four_keys.changes c ON changes = c.change_id
-LEFT JOIN(SELECT
-        incident_id,
-        change,
-        time_resolved
-        FROM four_keys.incidents i,
-        i.changes change) i ON i.change = changes
-GROUP BY day
+SELECT 
+  deployment_day,
+  IFNULL(incident_count, 0) as incident_count,
+  IFNULL(deployment_count, 0) as deployment_count,
+  IFNULL(incident_count, 0) / IFNULL(deployment_count, 0) AS change_failure_rate
+FROM
+  (SELECT 
+    TIMESTAMP_TRUNC(time_created, DAY) AS deployment_day,
+    COUNT(deploy_id) AS deployment_count
+  FROM 
+    four_keys.deployments
+  GROUP BY 
+    deployment_day) AS deploys_day
+LEFT JOIN 
+  (SELECT 
+    TIMESTAMP_TRUNC(time_created, DAY) AS incident_day,
+    COUNT(change_id) AS incident_count
+  FROM 
+    four_keys.incidents
+  GROUP BY 
+    incident_day) AS incidents_day
+ON 
+  deploys_day.deployment_day = incidents_day.incident_day
+ORDER BY 
+  deployment_day DESC;
 
